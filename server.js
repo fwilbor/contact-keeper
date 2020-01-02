5f955706a455e0109983463b36fbf34cd7e0c9c3
fwilbor <franklin.cfg@gmail.com>
2020-01-02 16:49:30 -0500

    update datebase with deprecation update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({msg: `Welcome tot the ContactKeeper API ...`}));
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.json({ msg: `Welcome tot the ContactKeeper API ...` }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
