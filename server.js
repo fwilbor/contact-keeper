584ef1c077ec53d4647a901f0e5e79481dd00550
fwilbor <franklin.cfg@gmail.com>
2020-01-07 22:15:12 -0500

    add express-validator

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Initialize Middleware
+app.use(express.json({ extended: false }));
+
+
 app.get('/', (req, res) => res.json({ msg: `Welcome to the ContactKeeper API ...` }));
 
 // Define Routes
