4b346edb7032f85ea1b49418e0b82a86466d1d12
fwilbor <franklin.cfg@gmail.com>
2020-01-07 19:44:08 -0500

    changed const to let UserSchema to get app working

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
+let UserSchema = new mongoose.Schema({
     name : {
         type: String,
         required: true
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('..');
+const User = require('../models/User');
 
 
 
